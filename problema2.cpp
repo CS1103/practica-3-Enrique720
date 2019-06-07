dbc6b0b1d1c88638bb4a5a2037f505bc96398b9a
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-06-07 13:14:46 -0500

    Update problema2.cpp

--- problema2.cpp
+++ problema2.cpp
@@ -4,46 +4,55 @@
 #include <deque>
 #include <time.h>
 #include <vector>
-
+#include <map>
+#include <fstream>
+#include <string>
+#include <iterator>
 using namespace std;
-template <typename T>
-deque<T> funcion(deque<T> a, int n ){
-    deque<T> retorno;
-    vector<T> temporal;
-    T temp = a.front();
-    unsigned long long int p= a.size();
-    for(int i = 0;i < p;i=i+n){
-        temp = a.front();
-           for(int j = 0;  j < n ; j++){
-               temporal.push_back(a.front());
-               a.pop_front();
-           }
-           for(int k = 0; k < n; k++){
-               if(temporal[k] <temp)
-                   temp = temporal[k];
-           }
-           retorno.push_back(temp);
-           for(int k = 0; k < n; k++){
-            temporal.pop_back();
-           }
+map<string, int> crear(){
+    map<string, int> retorno;
+    fstream file;
+    file.open("archivo.txt",ios::in);
+    string b;
+    int cont =0;
+    while(getline(file,b)){
+        cont++;
+    }
+    file.close();
+    file.open("archivo.txt",ios::in);
+    string a;
+    string key;
+    int value;
+    for(int i= 0; i < cont; i++ ){
+        file >> a;
+        if(a == "C"){
+               file >>key;
+               file >> value;
+               retorno.insert(pair<string,int>(key,value));
+
+        }
+        else if(a=="L"){
+            file >> key;
+
+            if(key =="All"){
+                for(auto it = retorno.cbegin(); it!=retorno.cend();++it){
+                    cout << it->first << "" << it->second << " " << endl;
+                }
+            }
+            else{
+                cout << retorno[key] << endl;
+            }
+        }
+        else if(a== "D"){
+            file >> key;
+            retorno.erase(key);
+
+        }
     }
     return retorno;
 }
 int main() {
-    deque<int>a;
-    a.push_front(1);
-    a.push_front(3);
-    a.push_front(9);
-    a.push_front(7);
-    a.push_front(10);
-    a.push_front(5);
-
-    deque<int> b;
-    b =funcion(a,2);
-    int j= b.size();
-    for(int i =0; i < j; i++)
-    {
-        cout << b.front() << " ";
-        b.pop_front();
-    }
+    fstream file;
+    map<string,int> a;
+    a = crear();
 }
